4a3dba92665e9c11dc54d3194c96ccef488b79cb
stepmuel <stephan@heap.ch>
2017-07-16 18:51:20 +0200

    Add missing content textbox.

--- assets/scripts/textboxes.js
+++ assets/scripts/textboxes.js
@@ -28,60 +28,68 @@ tinymce.PluginManager.add('textboxes', function (editor) {
         icon: false,
         menu: [
             { text: 'Formula', onclick: function () {
-				var selection = editor.selection.getContent();
-				if ( selection !== '' ) {
-                	editor.execCommand('mceReplaceContent', false, '<div class="textbox tbformel">\n'+selection+'</div><p></p>');
-				} else {
-					editor.execCommand('mceInsertContent', 0, '<div class="textbox tbformel"><p>Formula</p>\n</div><p></p>');
-				}
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="textbox tbformel">\n'+selection+'</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="textbox tbformel"><p>Formula</p>\n</div><p></p>');
+                }
             } },
             { text: 'How To', onclick: function () {
-				var selection = editor.selection.getContent();
-				if ( selection !== '' ) {
-                	editor.execCommand('mceReplaceContent', false, '<div class="textbox tbhowto"><h1 class="not-in-list">How To</h1>\n<p>'+selection+'</p>\n</div><p></p>');
-				} else {
-					editor.execCommand('mceInsertContent', 0, '<div class="textbox tbhowto"><h1 class="not-in-list">How To: </h1>\n<p>Text</p>\n</div><p></p>');
-				}
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="textbox tbhowto"><h1 class="not-in-list">How To</h1>\n<p>'+selection+'</p>\n</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="textbox tbhowto"><h1 class="not-in-list">How To: </h1>\n<p>Text</p>\n</div><p></p>');
+                }
             } },
             { text: 'Definition', onclick: function () {
-				var selection = editor.selection.getContent();
-				if ( selection !== '' ) {
-                	editor.execCommand('mceReplaceContent', false, '<div class="textbox tbdefinition">\n'+selection+'</div><p></p>');
-				} else {
-					editor.execCommand('mceInsertContent', 0, '<div class="textbox tbdefinition">\n<p>Definition</p>\n</div><p></p>');
-				}
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="textbox tbdefinition">\n'+selection+'</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="textbox tbdefinition">\n<p>Definition</p>\n</div><p></p>');
+                }
             } },
             { text: 'Example', onclick: function () {
-				var selection = editor.selection.getContent();
-				if ( selection !== '' ) {
-                	editor.execCommand('mceReplaceContent', false, '<div class="textbox tbbeispiel"><h1 class="not-in-list">Example</h1>\n<p>'+selection+'</p>\n</div><p></p>');
-				} else {
-					editor.execCommand('mceInsertContent', 0, '<div class="textbox tbbeispiel"><h1 class="not-in-list">Example</h1>\n<p>Text</p>\n</div><p></p>');
-				}
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="textbox tbbeispiel"><h1 class="not-in-list">Example</h1>\n<p>'+selection+'</p>\n</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="textbox tbbeispiel"><h1 class="not-in-list">Example</h1>\n<p>Text</p>\n</div><p></p>');
+                }
             } },
             { text: 'Question', onclick: function () {
-				var selection = editor.selection.getContent();
-				if ( selection !== '' ) {
-                	editor.execCommand('mceReplaceContent', false, '<div class="textbox tbfragen"><h1 class="not-in-list">Question</h1>\n<p>'+selection+'</p>\n</div><p></p>');
-				} else {
-					editor.execCommand('mceInsertContent', 0, '<div class="textbox tbfragen"><h1 class="not-in-list">Question</h1>\n<p>Text</p>\n</div><p></p>');
-				}
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="textbox tbfragen"><h1 class="not-in-list">Question</h1>\n<p>'+selection+'</p>\n</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="textbox tbfragen"><h1 class="not-in-list">Question</h1>\n<p>Text</p>\n</div><p></p>');
+                }
             } },
             { text: 'Reference', onclick: function () {
-				var selection = editor.selection.getContent();
-				if ( selection !== '' ) {
-                	editor.execCommand('mceReplaceContent', false, '<div class="textbox tbverweis">\n'+selection+'</div><p></p>');
-				} else {
-					editor.execCommand('mceInsertContent', 0, '<div class="textbox tbverweis">\n<p>Reference</p>\n</div><p></p>');
-				}
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="textbox tbverweis">\n'+selection+'</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="textbox tbverweis">\n<p>Reference</p>\n</div><p></p>');
+                }
             } },
             { text: 'Excursus', onclick: function () {
-				var selection = editor.selection.getContent();
-				if ( selection !== '' ) {
-                	editor.execCommand('mceReplaceContent', false, '<div class="textbox tbexkurs"><h1 class="not-in-list">Excursus</h1>\n<p>'+selection+'</p>\n</div><p></p>');
-				} else {
-					editor.execCommand('mceInsertContent', 0, '<div class="textbox tbexkurs"><h1 class="not-in-list">Excursus</h1>\n<p>Text</p>\n</div><p></p>');
-				}
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="textbox tbexkurs"><h1 class="not-in-list">Excursus</h1>\n<p>'+selection+'</p>\n</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="textbox tbexkurs"><h1 class="not-in-list">Excursus</h1>\n<p>Text</p>\n</div><p></p>');
+                }
+            } },
+            { text: 'Export Only', onclick: function () {
+                var selection = editor.selection.getContent();
+                if ( selection !== '' ) {
+                    editor.execCommand('mceReplaceContent', false, '<div class="not-web textbox tbexkurs"><h1 class="not-in-list">Missing Content</h1>\n<p>'+selection+'</p>\n</div><p></p>');
+                } else {
+                    editor.execCommand('mceInsertContent', 0, '<div class="not-web textbox tbexkurs"><h1 class="not-in-list">Missing Content</h1>\n<p>Visit <a href="https://eskript.ethz.ch/" target="_blank">eskript.ethz.ch</a> to see everything.</p>\n</div><p></p>');
+                }
             } },
         ]
     });
