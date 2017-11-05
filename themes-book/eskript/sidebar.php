d87dd577140b72b64512e762cf4667b307f3d413
sarahfrederickx <sarah@frederickx.ch>
2017-11-05 19:24:07 +0100

    always show subchapters
    in ToC in chapters

--- themes-book/eskript/sidebar.php
+++ themes-book/eskript/sidebar.php
@@ -23,7 +23,8 @@
 <?php
 
 $options = get_option( 'pressbooks_theme_options_global' );
-$levels = @$options['toc_levels'];
+$levels = 2; //always enable two level toc on subchapter page because the chapters wont be displayed else
+// $levels = @$options['toc_levels'];
 
 if ($levels > 1) {
 	$toc = eskript_post_toc(null, $levels);
